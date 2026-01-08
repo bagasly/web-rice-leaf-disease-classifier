41055bdddec557787790a72464c7533192956eb4
bagas <bagasly33@gmail.com>
2026-01-09 04:10:33 +0700

    fix text

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def load_model(model_choice):
 
 # Pilihan model untuk digunakan
 model_choice = st.radio(
-    "Pilih Model yang Ingin Digunakan:",
+    "## Pilih Model yang Ingin Digunakan:",
     ("Model Baseline", "Model Terbaik")
 )
 
@@ -21,7 +21,7 @@ model_choice = st.radio(
 model = load_model(model_choice)
 
 # Menampilkan informasi model yang dipilih
-st.write(f"Model yang dipilih: {model_choice}")
+st.write(f"## Model yang dipilih: {model_choice}")
 
 # Memuat model sesuai pilihan
 model = load_model(model_choice)
